053965a72dcd3f32a21541a156ba044718ef6df9
Louis Fisher <37254732+LFISHER7@users.noreply.github.com>
2022-08-12 16:46:22 +0100

    switch to using days for prescription window

--- analysis/prescription_variables.py
+++ analysis/prescription_variables.py
@@ -5,22 +5,22 @@ prescription_variables = dict(
 
     steroids = patients.with_these_medications(
         steroid_codelist,
-        between=["index_date - 6 months", "index_date"],
+        between=["index_date - 180 days", "index_date"],
         returning="binary_flag",
     ),
     antibiotics = patients.with_these_medications(
         antibiotic_codelist,
-        between=["index_date - 6 months", "index_date"],
+        between=["index_date - 180 days", "index_date"],
         returning="binary_flag",
     ),
     antivirals = patients.with_these_medications(
         antiviral_codelist,
-        between=["index_date - 6 months", "index_date"],
+        between=["index_date - 180 days", "index_date"],
         returning="binary_flag",
     ),
     statins = patients.with_these_medications(
         statin_codelist,
-        between=["index_date - 6 months", "index_date"],
+        between=["index_date - 180 days", "index_date"],
         returning="binary_flag",
     )
 
