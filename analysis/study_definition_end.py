07ff432bb77fb12ed2ff367e5ef1f01918288210
Louis Fisher <37254732+LFISHER7@users.noreply.github.com>
2022-09-20 17:08:00 +0100

    fix study def end date

--- analysis/study_definition_end.py
+++ analysis/study_definition_end.py
@@ -6,7 +6,7 @@ from cohortextractor import (
 from codelists import *
 from vaccination_variables import vaccination_variables
 
-end_date = "2022-07-01"
+end_date = "2021-06-30"
 
 
 study = StudyDefinition(
