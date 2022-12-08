11b09d69c340e7747485ce9fc828e442e15e0130
Louis Fisher <37254732+LFISHER7@users.noreply.github.com>
2022-12-08 14:47:30 +0000

    show number with numeric value

--- analysis/test_column_counts.py
+++ analysis/test_column_counts.py
@@ -9,13 +9,26 @@ df = pd.read_csv('output/input_test.csv.gz')
 counts = []
 
 for col in df.columns:
-    if col.startswith('flucats') and col.endswith('code'):
-        count = (df[col].value_counts())
+    if col.startswith('flucats_question'):
+        if col.endswith('code'):
         
+            count = (df[col].value_counts())
+            
+        elif col.endswith('value'):
+            count = pd.Series((df[col] != 0).count())
+            
+        else:
+            count = pd.Series([0])
+        
+        # print(count)
         # set index of count to multiindex of column name and code
         count.index = pd.MultiIndex.from_product([[col], count.index])
         counts.append(count)
 
 count_comined = pd.concat(counts)
-count_comined.to_csv('output/column_counts/combined.csv', header=True, index=True)
+# set any values <100 and >0 to 100
+count_comined = count_comined.apply(lambda x: 100 if x < 100 and x > 0 else x)
+# round to nrearest 100
+count_comined = count_comined.apply(lambda x: round(x, -2))
 
+count_comined.to_csv('output/column_counts/combined.csv', header=True, index=True)
