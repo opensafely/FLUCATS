429c870603f85f3b50af7a162bf7c1ae09522e2d
Louis Fisher <37254732+LFISHER7@users.noreply.github.com>
2022-12-12 18:39:46 +0000

    reinstate all cohort extraction actions

--- analysis/combine_input_files.py
+++ analysis/combine_input_files.py
@@ -10,21 +10,21 @@ def chunk_and_filter(file):
     return pd.concat(filtered)
 
 
-df_1 = chunk_and_filter('output/input_2020-04-01.csv.gz')
+df_1 = chunk_and_filter('output/joined/input_2020-04-01.csv.gz')
 
 # all files in output that start with input and end with .csv.gz and don't contain 'end' and are not input_2020-04-01.csv.gz
 files = [f for f in os.listdir('output') if f.startswith('input') and f.endswith('.csv.gz') and not f.startswith('input_2020-04-01') and not f.endswith('end.csv.gz')]
 
 for file in files:
     # read in the file in chunks with pandas
-    df_2 = chunk_and_filter(f'output/{file}')
+    df_2 = chunk_and_filter(f'output/joined/{file}')
 
     
     df_1 = pd.concat([df_1, df_2])
 
 
 
-df_1.to_csv('output/input_all_py.csv.gz', index=False)
+df_1.to_csv('output/joined/input_all_py.csv.gz', index=False)
 
 
 
