cda87d55716a4d4040d2b2143f1ad990360202be
Louis Fisher <louis.fisher1919@gmail.com>
2023-06-23 12:36:33 +0100

    fix numeric proportion

--- analysis/analysis/check_numeric_values.py
+++ analysis/analysis/check_numeric_values.py
@@ -5,44 +5,47 @@ from pathlib import Path
 def main():
 
     numeric_variables_list = [
-        "flucats_question_numeric_value_dehydration_or_shock_15527001_value",
-        "flucats_question_numeric_value_dehydration_or_shock_787041000000101_value",
-        "flucats_question_numeric_value_dehydration_or_shock_787051000000103_value",
-        "flucats_question_numeric_value_heart_rate_422119006_value",
-        "flucats_question_numeric_value_heart_rate_429525003_value",
-        "flucats_question_numeric_value_heart_rate_429614003_value",
-        "flucats_question_numeric_value_heart_rate_78564009_value",
-        "flucats_question_numeric_value_heart_rate_843941000000100_value",
-        "flucats_question_numeric_value_respiratory_rate_250810003_value",
-        "flucats_question_numeric_value_respiratory_rate_271625008_value",
-        "flucats_question_numeric_value_respiratory_rate_86290005_value",
-        "flucats_question_numeric_value_respiratory_rate_927961000000102_value",
-        "flucats_question_numeric_value_oxygen_saturation_431314004_value",
-        "flucats_question_numeric_value_oxygen_saturation_927981000000106_value",
-        "flucats_question_numeric_value_temperature_703421000_value",
+        "flucats_question_numeric_value_dehydration_or_shock_15527001",
+        "flucats_question_numeric_value_dehydration_or_shock_787041000000101",
+        "flucats_question_numeric_value_dehydration_or_shock_787051000000103",
+        "flucats_question_numeric_value_heart_rate_422119006",
+        "flucats_question_numeric_value_heart_rate_429525003",
+        "flucats_question_numeric_value_heart_rate_429614003",
+        "flucats_question_numeric_value_heart_rate_78564009",
+        "flucats_question_numeric_value_heart_rate_843941000000100",
+        "flucats_question_numeric_value_respiratory_rate_250810003",
+        "flucats_question_numeric_value_respiratory_rate_271625008",
+        "flucats_question_numeric_value_respiratory_rate_86290005",
+        "flucats_question_numeric_value_respiratory_rate_927961000000102",
+        "flucats_question_numeric_value_oxygen_saturation_431314004",
+        "flucats_question_numeric_value_oxygen_saturation_927981000000106",
+        "flucats_question_numeric_value_temperature_703421000",
     ]
 
-    proportions_numeric = []
+    codes = []
+
+    for variable in numeric_variables_list:
+        codes.append(variable.split("_")[-1])
+
+    numeric_values_variables_list = [f"{x}_value" for x in numeric_variables_list]
+
     file_path = Path("output/joined/full/input_all.csv")
 
-    df = pd.read_csv(file_path, usecols=numeric_variables_list, dtype="float")
-    df_dates = pd.read_csv(file_path, usecols=["date"])
+    df = pd.read_csv(file_path, usecols=numeric_variables_list.extend(numeric_values_variables_list))
 
-    df = pd.concat([df, df_dates], axis=1)
-
-    # for each column, calculate proportion where value is not null
-
+    
     proportions = {}
-    unique_dates = df["date"].unique()
-    for column in df.columns:
-        if column != "date":
-            for date in unique_dates:
-                proportions[column] = round(len(df.loc[(df["date"]==date) & (df[column] > 0)]) / len(df), 2)
 
-    proportions_numeric.append(proportions)
+    for code in codes:
+    
+        binary_variable = [x for x in numeric_variables_list if code in x][0]
+        numeric_value = [x for x in numeric_values_variables_list if code in x][0]
 
-    # mean value across months for each variable
-    proportions_numeric = pd.DataFrame(proportions_numeric).mean()
+        subset_df = df.loc[df[binary_variable] == 1]
+
+        proportions[code] = round(len(subset_df.loc[subset_df[numeric_value] > 0]) / len(subset_df), 2)
+
+    proportions_numeric = pd.DataFrame(proportions, index=["proportion"])
 
     proportions_numeric.to_csv("output/joined/full/proportions_numeric.csv")
 
