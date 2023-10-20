cee1f85e53d57a0114402094f7ddc16310e944be
Louis Fisher <louis.fisher1919@gmail.com>
2023-10-20 12:36:37 +0100

    drop unused vacc vars

--- analysis/variables/vaccination_variables.py
+++ analysis/variables/vaccination_variables.py
@@ -2,75 +2,7 @@ from cohortextractor import patients
 from codelists import *
 
 vaccination_variables = dict(
-    first_vaccination=patients.with_these_clinical_events(
-        vaccination_codelist,
-        between=["2020-12-01", "2021-06-30"],
-        returning="binary_flag",
-        find_first_match_in_period=True,
-        include_date_of_match=True,
-        date_format="YYYY-MM-DD",
-        return_expectations={
-            "incidence": 0.8,
-        },
-    ),
-    second_vaccination=patients.with_these_clinical_events(
-        vaccination_codelist,
-        between=["first_vaccination_date + 19 days", "2021-06-30"],
-        returning="binary_flag",
-        find_first_match_in_period=True,
-        include_date_of_match=True,
-        date_format="YYYY-MM-DD",
-        return_expectations={
-            "incidence": 0.8,
-        },
-    ),
-    coded_second_vaccination=patients.with_these_clinical_events(
-        second_dose_codelist,
-        between=["2020-12-01", "2021-06-30"],
-        returning="binary_flag",
-        find_first_match_in_period=True,
-        include_date_of_match=True,
-        date_format="YYYY-MM-DD",
-        return_expectations={
-            "incidence": 0.8,
-        },
-    ),
-    booster_vaccination=patients.with_these_clinical_events(
-        vaccination_codelist,
-        between=["second_vaccination_date + 56 days", "2021-06-30"],
-        returning="binary_flag",
-        find_first_match_in_period=True,
-        include_date_of_match=True,
-        date_format="YYYY-MM-DD",
-        return_expectations={
-            "incidence": 0.8,
-        },
-    ),
-    coded_booster_vaccination=patients.with_these_clinical_events(
-        booster_dose_codelist,
-        between=["2020-12-01", "2021-06-30"],
-        returning="binary_flag",
-        find_first_match_in_period=True,
-        include_date_of_match=True,
-        date_format="YYYY-MM-DD",
-        return_expectations={
-            "incidence": 0.8,
-        },
-    ),
-    has_second_dose = patients.satisfying(
-        """
-        second_vaccination OR
-        coded_second_vaccination
-        """,
-    ),
-    has_booster_dose = patients.satisfying(
-        """
-        booster_vaccination OR
-        coded_booster_vaccination
-        """,
-    ),
-
-
+    
      # First COVID vaccination administration codes
     covadm1_dat=patients.with_vaccination_record(
         returning="date",
