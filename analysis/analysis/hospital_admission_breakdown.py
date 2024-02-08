7040add80e59dafe6721606836f936b04fedba30
Louis Fisher <louis.fisher1919@gmail.com>
2024-02-08 14:30:53 +0000

    print hosp length

--- analysis/analysis/hospital_admission_breakdown.py
+++ analysis/analysis/hospital_admission_breakdown.py
@@ -4,7 +4,7 @@ import pandas as pd
 df = pd.read_csv('output/input_all_edited.csv')
 
 hosp_within_24hrs = df[df["hosp_24h"] == 1]
-
+print(len(hosp_within_24hrs))
 codes = hosp_within_24hrs["hospital_admission_code"].value_counts()
 
 codes.to_csv('output/results/hospital_admission_breakdown.csv')
