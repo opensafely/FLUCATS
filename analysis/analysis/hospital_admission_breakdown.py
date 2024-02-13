b84dfa23d98ed819312336000aeaf4ae6cd35864
Louis Fisher <louis.fisher1919@gmail.com>
2024-02-13 10:15:07 +0000

    breakdown prob/susp cov counts

--- analysis/analysis/hospital_admission_breakdown.py
+++ analysis/analysis/hospital_admission_breakdown.py
@@ -11,17 +11,36 @@ df["suspected_covid"] = (
 df["probable_covid"] = (
     df.filter(regex="flucats_question_probable_covid").eq(1).any(axis=1)
 )
+
+
+# subset to only columns related to probable covid
+probable_subset = df.filter(regex="flucats_question_probable_covid")
+suspected_subset = df.filter(regex="flucats_question_suspected_covid")
+
+# value counts of each column
+probable_counts = probable_subset.apply(pd.Series.value_counts)
+suspected_counts = suspected_subset.apply(pd.Series.value_counts)
+
+# remove columns with less than 7 counts and round to nearest 5
+probable_counts[probable_counts <= 7] = np.nan
+probable_counts = probable_counts.dropna()
+probable_counts = probable_counts.apply(lambda x: round(x / 5) * 5)
+
+suspected_counts[suspected_counts <= 7] = np.nan
+suspected_counts = suspected_counts.dropna()
+suspected_counts = suspected_counts.apply(lambda x: round(x / 5) * 5)
+
+# save to csv
+probable_counts.to_csv("output/results/probable_covid_value_counts.csv")
+suspected_counts.to_csv("output/results/suspected_covid_value_counts.csv")
+
+
 df["suspected_covid"] = df["suspected_covid"].astype(int)
 df["probable_covid"] = df["probable_covid"].astype(int)
 
 
-df["probable_or_suspected_covid"] = (df["probable_covid"] == 1) | (
-    df["suspected_covid"] == 1
-)
-df["probable_or_suspected_covid"] = df["probable_or_suspected_covid"].astype(int)
-
 # save df with value counts of each column - prob covid, suspected covid, prob or suspected covid
-df[["probable_covid", "suspected_covid", "probable_or_suspected_covid"]].apply(
+df[["probable_covid", "suspected_covid"]].apply(
     pd.Series.value_counts
 ).to_csv("output/results/cov_status_value_counts.csv")
 
@@ -36,7 +55,7 @@ hosp_codes = hosp_codes.dropna()
 
 
 counts = hosp_within_24hrs.groupby(
-    ["probable_covid", "suspected_covid", "probable_or_suspected_covid"]
+    ["probable_covid", "suspected_covid"]
 )["hosp_24h"].value_counts()
 
 counts.to_csv("output/results/hospital_admission_cov_status.csv")
