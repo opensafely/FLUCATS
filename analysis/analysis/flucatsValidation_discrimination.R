96e75a677d9fa63e6c0af7bb4dcc50577a8b0e4d
Louis Fisher <louis.fisher1919@gmail.com>
2024-02-16 13:21:24 +0000

    fix auc discrim

--- analysis/analysis/flucatsValidation_discrimination.R
+++ analysis/analysis/flucatsValidation_discrimination.R
@@ -71,7 +71,7 @@ if (!is.null(hosp_child_prob_cov)) {
 
   auc_hosp_child_prob_cov <- auc(mroc_hosp_child_prob_cov)
   auc_hosp_child_prob_cov_ci <- ci.auc(mroc_hosp_child_prob_cov)
-  auc_hosp_child_prob_cov_ci_str <-  paste("(", round(auc_hosp_child_prob_cov_ci[1], 2), "-", round(auc_hosp_prob_cov_child_ci[2], 2), ")", sep = "")
+  auc_hosp_child_prob_cov_ci_str <-  paste("(", round(auc_hosp_child_prob_cov_ci[1], 2), "-", round(auc_hosp_child_prob_cov_ci[2], 2), ")", sep = "")
 
 } else {
   write.csv(data.frame(), "output/results/roc_data_hosp_child_prob_cov.csv")
