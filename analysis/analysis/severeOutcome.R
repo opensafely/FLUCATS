2883b57ceac2e2198738d0ef19e94893f9676f0e
Louis Fisher <louis.fisher1919@gmail.com>
2024-02-18 18:04:38 +0000

    more diagnostics

--- analysis/analysis/severeOutcome.R
+++ analysis/analysis/severeOutcome.R
@@ -116,6 +116,8 @@ if (!is.null(severe_o)) {
   # print value counts for the severe outcome and the prediction
   print(table(df_adult$severe_outcome))
   print(table(df_adult$prediction_severe_outcome > 0.5))
+  print(df_adult$prediction_severe_outcome)
+  
 
   output <- calibration_plot(data = df_adult, obs = "severe_outcome", pred = "prediction_severe_outcome", data_summary=T)
 
